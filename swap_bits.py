2bbaa5261901d63fcec00c1c272f8be40d8feeca
kaczifant <35376563+kaczifant@users.noreply.github.com>
2019-09-02 08:02:31 +0200

    Add files via upload

---
+++ swap_bits.py
@@ -0,0 +1,22 @@
+from test_framework import generic_test
+
+
+def swap_bits(x, i, j):
+    i_th = (x & 1 << i) >> i
+    j_th = (x & 1 << j) >> j
+        
+    if i_th != j_th:
+        if i_th == 0:
+            x = (x | (1 << i)) & ~(1 << j) 
+        elif j_th == 0:
+            x = (x | (1 << j)) & ~(1 << i)
+        return x
+    else:
+        return x
+    
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main("swap_bits.py", 'swap_bits.tsv',
+                                       swap_bits))
